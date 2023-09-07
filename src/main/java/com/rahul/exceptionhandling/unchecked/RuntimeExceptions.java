1de38257db13d73fcba62d17801a27026818a0a6
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-09-08 04:27:10 +0530

    RuntimeExceptions all commented , remove comment and test

---
+++ src/main/java/com/rahul/exceptionhandling/unchecked/RuntimeExceptions.java
@@ -0,0 +1,33 @@
+package com.rahul.exceptionhandling.unchecked;
+
+public class RuntimeExceptions {
+    public static void main(String[] args) {
+        //Object val= 5;
+        //System.out.println((String) val);
+
+        //ClassCastException . this can also happen during serialization and deserialization
+
+        //System.out.println(1/0);
+
+        //this causes Arithmetic exception
+
+        //int [] arr= new int[2];
+        //System.out.println(arr[2]);
+
+        //ArrayIndexOutOfBoundsException
+
+        //tring str="abc";
+        //System.out.println(str.charAt(3));
+
+        //StringIndexOutOfBoundsException
+
+        //String str2 = null;
+        //System.out.println(str2.charAt(0));
+
+        //NullPointerException
+
+        //Integer intval= Integer.parseInt("abc");
+
+        //Illegal argument exception , number format exception
+    }
+}
